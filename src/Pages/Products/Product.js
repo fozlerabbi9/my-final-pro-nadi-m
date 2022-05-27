c4b8ea33a2cd78336daedbdc4df5355146d59258
Nadim Mahmud <giftsframebd@gmail.com>
2022-05-27 16:24:39 +0600

    scroll to top

--- src/Pages/Products/Product.js
+++ src/Pages/Products/Product.js
@@ -6,7 +6,7 @@ const Product = ({ product, refetch }) => {
     return (
         <div className='flex justify-center'>
 
-            <div className="card perItem w-96 bg-base-100 shadow-xl" style={{ height: '100%' }} data-aos="fade-up">
+            <div className="card orderCard perItem w-96 bg-base-100 shadow-xl" style={{ height: '100%' }} data-aos="fade-up">
                 <figure className="px-10 pt-10">
                     <img src={product.img} alt="Shoes" className="rounded-xl" style={{width:"220px"}} />
                 </figure>
