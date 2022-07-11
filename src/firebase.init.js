339aaf2a34b141b86c319c3515d272216afcb827
Fozle Rabbi <fozlerabbi9790@gmail.com>
2022-07-11 15:05:32 +0600

    fixed error

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
   appId:process.env.REACT_APP_appId 
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 export default auth
